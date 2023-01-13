a732b167747b0db89d9fbb54745eafc9e9ede109
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2023-01-12 19:22:29 -0500

    PUT endpoint can update name and email based on id

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -41,6 +43,28 @@ public class StudentService {
             throw new IllegalStateException("Student with id " + studentId + " does not exist in our database.");
         } else {
             studentRepository.deleteById(studentId);
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("error"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+            System.out.println("Name successfully updated!");
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException("Email already taken");
+            }
+
+            student.setEmail(email);
+            System.out.println("Email successfully updated!");
         }
     }
 
